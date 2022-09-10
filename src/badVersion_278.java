5b86b616a0d7a61fa13410ec116f0e1033150533
Mohammad Sami <mohammadsami@duck.com>
2022-09-10 20:32:21 +0530

    Solve: problem no. 278

---
+++ src/badVersion_278.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class badVersion_278 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int bad = input.nextInt();
+        int ans = firstBadVersion(n, bad);
+        System.out.println(ans);
+    }
+
+    private static int firstBadVersion(int n, int bad) {
+        int start = 0;
+        int end = n;
+        while(start < end ){
+
+            int mid = start + (end - start)/2;
+            boolean res = isBadVersion(mid, bad);
+            if(res){
+                end = mid ;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static boolean isBadVersion(int n, int bad){
+        return n == bad || n > bad;
+    }
+}
